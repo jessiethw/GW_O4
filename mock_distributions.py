3d0f865e93c7f5d96d734402902f1d25ad2eccf9
jessiethw <thwaites@wisc.edu>
2022-08-17 15:59:23 -0500

    match fra pval dist

--- mock_distributions.py
+++ mock_distributions.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import glob
 import pickle
 from datetime import date
+from fast_response import web_utils
 
 def make_bg_pval_dist(fontsize=15):
     all_maps_saved_pkl=sorted(glob.glob('/data/user/jthwaites/o4-mocks/*/*.pickle'))[::-1]
@@ -31,8 +32,8 @@ def make_bg_pval_dist(fontsize=15):
     print('Done loading mocks.')
 
     mpl.rcParams.update({'font.size':fontsize})
-    fig, ax = plt.subplots(figsize =(10, 7))
-    ax.tick_params(labelsize=fontsize)
+    plt.figure(figsize = (10,6), dpi=300)
+    #ax.tick_params(labelsize=fontsize)
 
     p_x_vals = np.logspace(-2.5,0.,16)
     n, bins, patches = plt.hist([all_mocks[name] for name in all_mocks.keys()], 
@@ -41,10 +42,10 @@ def make_bg_pval_dist(fontsize=15):
     lt_10per = sum(n[bins[:-1]<=0.1])
     lt_1per=sum(n[bins[:-1]<=0.01])
     
-    plt.plot([0.1,0.1], [1e-3, 1e0],linestyle='dotted', label='%.1f %% of p-values < 0.1'%(lt_10per*100.))
-    plt.plot([0.01, 0.01], [1e-3, 1e0], linestyle='dashed',label='%.1f %% of p-values < 0.01'%(lt_1per*100.))
-
     plt.step(p_x_vals[1:], np.diff(p_x_vals), label = 'Uniform p-value expectation', lw = 3.)
+    plt.plot([0.1,0.1], [1e-3, 1e0],linestyle='dotted', label=f'{lt_10per*100.:.2f} \% of p-values $<$ 0.1')
+    plt.plot([0.01, 0.01], [1e-3, 1e0], linestyle='dashed',label=f'{lt_1per*100.:.2f} \% of p-values $<$ 0.01')
+
     plt.xscale('log')
     plt.yscale('log')
     plt.gca().invert_xaxis()
@@ -54,10 +55,10 @@ def make_bg_pval_dist(fontsize=15):
     plt.xlabel('p-value', fontsize = fontsize)
     plt.ylabel('Fraction of Analyses', fontsize = fontsize)
     plt.legend(loc = 1, fontsize = fontsize)
-    plt.title('Histogram of %i mock p-values as of %s'%(len(saved_mock_pkl),str(date.today())))
+    plt.title('{} Mock p-values as of {}'.format(len(saved_mock_pkl), str(date.today()),fontsize=20))
 
     save_path='/data/user/jthwaites/o4-mocks/mock_pvals_%s.png'%str(date.today())
-    plt.savefig(save_path)
+    plt.savefig(save_path, dpi=200, bbox_inches='tight')
     print('Figure saved to file: ', save_path)
     
 make_bg_pval_dist()
