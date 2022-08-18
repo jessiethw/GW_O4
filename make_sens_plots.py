1b779b7504a6becc7a40012d9de725579211776c
jessiethw <thwaites@wisc.edu>
2022-08-18 11:30:08 -0500

    finish writing sens calc for case with prior

--- make_sens_plots.py
+++ make_sens_plots.py
@@ -37,6 +37,30 @@ p.add_argument("--with_map", default=False, type=bool,
                 help='calculate a sensitivity for a gw event with a spatial prior map')
 args = p.parse_args()
 
+def calc_sensitivty(passing_frac, flux_inj):
+    passing = np.array(passing_frac, dtype=float)
+    errs = sensitivity_utils.binomial_error(passing, 1000.)
+    fits, plist = [], []
+    for func, func_name in [(sensitivity_utils.chi2cdf, 'chi2'),
+                            (sensitivity_utils.erfunc, 'Error function'),
+                            (sensitivity_utils.incomplete_gamma, 'Incomplete gamma')]:
+        try:
+            sens_fit=sensitivity_utils.sensitivity_fit(flux_inj, passing, errs, func)
+            if str(sens_fit['chi2']) != 'nan' and str(sens_fit['pval']) != 'nan':
+                fits.append(sens_fit)
+                plist.append(fits[-1]['pval'])
+        except:
+            print(f"{func_name} fit failed in upper limit calculation")
+
+    plist = np.array(plist)
+    best_fit_ind= np.argmax(plist)
+    
+    sensitivity=fits[best_fit_ind]['sens']
+    fits[best_fit_ind]['ls'] = '-'
+
+    print('Sensitivity at dec = %.1f: %.2f events'%(dec, sensitivity))
+    return sensitivity, fits, errs
+
 reload=args.reload_sens
 if args.with_map:
     reload=True #don't recalculate ps sensitivities
@@ -44,7 +68,6 @@ if args.with_map:
 fontsize=args.fontsize
 sensitivity_flux=[]
 sensitivity_ns=[]
-fit_names=[]
 #decs=[-67.5, -45., -22.5, 0., 22.5, 45., 67.5]
 decs= np.linspace(-85,85,35)
 
@@ -66,29 +89,8 @@ if not reload:
                 ns_inj.append(result['ns_inj'])
 
         #### Calculate sensitivity #####
-        passing = np.array(passing_frac[:16], dtype=float)
-        errs = sensitivity_utils.binomial_error(passing, 1000.)
-        fits, plist = [], []
-        for func, func_name in [(sensitivity_utils.chi2cdf, 'chi2'),
-                                (sensitivity_utils.erfunc, 'Error function'),
-                                (sensitivity_utils.incomplete_gamma, 'Incomplete gamma')]:
-            try:
-                sens_fit=sensitivity_utils.sensitivity_fit(ns_inj[:16], passing, errs, func)
-                if str(sens_fit['chi2']) != 'nan' and str(sens_fit['pval']) != 'nan':
-                    fits.append(sens_fit)
-                    plist.append(fits[-1]['pval'])
-            except:
-                print(f"{func_name} fit failed in upper limit calculation")
-
-        plist = np.array(plist)
-        best_fit_ind= np.argmax(plist)
-    
-        #n_inj_range=np.arange(0., ns_inj[16], 0.1)
-        sensitivity=fits[best_fit_ind]['sens']
-        fits[best_fit_ind]['ls'] = '-'
-        fit_names.append(fits[best_fit_ind]['name'])
-
-        print('Sensitivity at dec = %.1f: %.2f events'%(dec, sensitivity))
+        sensitivity, fits, errs = calc_sensitivty(passing_frac[:16], ns_inj[:16])
+        
         llh = config(['GFUOnline_v001p03','IC86, 2011-2018'],gamma=2.,ncpu=2, days=5,
               time_mask=[500./3600./24.,57982.52852350], poisson=True)
 
@@ -113,7 +115,7 @@ if not reload:
                 ax.axvline(fit_dict['sens'], color = 'm', linewidth = 0.3, linestyle = '-.')
                 ax.text(3.5, 0.8, 'Sens. = {:.2f} events'.format(fit_dict['sens']), fontsize = fontsize)
                 ax.text(3.5, 0.7, ' = {:.1e}'.format(inj.mu2flux(sensitivity)*1e9) + r' GeV cm$^-2$', fontsize=fontsize)
-        ax.errorbar(ns_inj[:16], passing, yerr=errs, capsize = 3, linestyle='', marker = 's', markersize = 2)
+        ax.errorbar(ns_inj[:16], passing_frac[:16], yerr=errs, capsize = 3, linestyle='', marker = 's', markersize = 2)
         ax.legend(loc=0, fontsize = fontsize)
         ax.set_xlabel('n inj', fontsize = fontsize)
         ax.set_ylabel(r'Fraction TS $>$ threshold', fontsize = fontsize)
@@ -139,8 +141,7 @@ if not reload:
     with open('./calculated_sensitivities.pickle','wb') as f:
         pickle.dump({'dec': decs,
                  'sens_ns':sensitivity_ns,
-                 'sens_flux':sensitivity_flux,
-                 'best_fit_name':fit_names}, f)
+                 'sens_flux':sensitivity_flux}, f)
 else: 
     print('Reloading calculated sensitivities')
 
@@ -155,18 +156,54 @@ else:
 
 if args.with_map:
     sens_trials=[f'./sens_trials/S191216ap/prior_sens_trials_{str(pid)}.pkl' 
-                    for pid in range(0,200)]
+                    for pid in range(0,50)]
     passing_frac=[]
-    ns_fit_mean=[]
-    ns_fit_1sigma=[]
-    ns_inj=[]
+    flux_fit_mean=[]
+    flux_fit_1sigma=[]
+    flux_inj=[]
     for i in range(len(sens_trials)):
         with open(sens_trials[i], 'rb') as f:
                 result=pickle.load(f)
                 passing_frac.append(result['passFrac'][0])
-                ns_fit_mean.append(np.mean(result['ns_fit']))
-                ns_fit_1sigma.append(np.std(result['ns_fit']))
-                ns_inj.append(result['ns_inj'])
+                flux_fit_mean.append(np.mean(result['flux_fit'])*1e9)
+                flux_fit_1sigma.append(np.std(result['flux_fit']))
+                flux_inj.append(result['flux_inj']*1e9)
+
+    #### Calculate sensitivity #####
+    sensitivity, fits = calc_sensitivty(passing_frac[:16], flux_inj[:16])
+
+    mpl.rcParams.update({'font.size':fontsize})
+    fig,ax = plt.subplots(figsize = (10,6))
+    ax.tick_params(labelsize=fontsize)    
+
+    for fit_dict in fits:
+        label=r'{}: $\chi^2$ = {:.2f}, d.o.f. = {}'.format(fit_dict['name'], fit_dict['chi2'], fit_dict['dof'])
+        ax.plot(fit_dict['xfit'], fit_dict['yfit'], label = label, ls = fit_dict['ls'])
+        if fit_dict['ls'] == '-':
+            ax.axhline(0.9, color = 'm', linewidth = 0.3, linestyle = '-.')
+            ax.axvline(fit_dict['sens'], color = 'm', linewidth = 0.3, linestyle = '-.')
+            ax.text(3.5, 0.8, 'Sens. = {:.2f}'.format(fit_dict['sens'])+ r' GeV cm$^-2$', fontsize = fontsize)
+    ax.errorbar(flux_inj[:20], passing_frac[:20], yerr=errs, capsize = 3, linestyle='', marker = 's', markersize = 2)
+    ax.legend(loc=0, fontsize = fontsize)
+    ax.set_xlabel('flux inj', fontsize = fontsize)
+    ax.set_ylabel(r'Fraction TS $>$ threshold', fontsize = fontsize)
+
+    plt.title(f'Passing fraction for S191216ap')
+    plt.savefig(f'./plots/S191216ap_passing_frac.png')
+    
+    ### Make bias plot ###
+    plt.clf()
+    plt.plot(flux_inj,flux_fit_mean)
+    plt.plot(flux_inj,flux_inj,'--', color='C0')
+    plt.fill_between(flux_inj, 
+                    [flux_fit_mean[i]-flux_fit_1sigma[i] for i in range(len(flux_fit_mean))], 
+                    [flux_fit_mean[i]+flux_fit_1sigma[i] for i in range(len(flux_fit_mean))], 
+                    alpha=0.2)
+
+    plt.xlabel('flux inj')
+    plt.ylabel('flux fit')
+    plt.title(f'Bias for S191216ap spatial prior')
+    plt.savefig(f'./plots/S191216ap_bias.png')
 
 plt.clf()
 dec_range = np.linspace(-1,1,35)
